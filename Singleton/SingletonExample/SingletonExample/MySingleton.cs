024625e8cb162e91eb5abcb0e21f11462aa9701a
Edward Eisenhart08 <EdwardEisenhart08@gmail.com>
2015-03-15 23:25:51 -0700

    Adding singleton example code

---
+++ Singleton/SingletonExample/SingletonExample/MySingleton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+
+    /// <summary>
+    /// Example of a singleton.
+    /// Created By: Edward Eisenhart
+    /// Created On: Mar-15-2015
+    /// Contact: EdwardEisenhart@EdwardEisenhart.com
+    /// </summary>
+    public sealed class MySingleton
+    {
+        private static MySingleton instance;
+
+        /// <summary>
+        /// Retreives the instance of <see cref="MySingleton"/>
+        /// </summary>
+        public static MySingleton Instance 
+        {
+            get 
+            {
+                if (instance == null)
+                {
+                    instance = new MySingleton();
+                }
+
+                return instance;
+            } 
+        }
+
+        /// <summary>
+        /// Since there is a private constructor, no public default constructor is created
+        /// and there is no way to constuct an instance of the singleton from another class.
+        /// This ensures only 1 instance of the singleton exists.
+        /// </summary>
+        private MySingleton() { }
+
+        /// <summary>
+        /// Example instance method of the singleton
+        /// </summary>
+        public void ExampleMethod()
+        {
+
+        }
+
+    }
+}
---
+++ Singleton/SingletonExample/SingletonExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    /// <summary>
+    /// Example usage of the singleton.
+    /// Created By: Edward Eisenhart
+    /// Created On: Mar-15-2015
+    /// Contact: EdwardEisenhart@EdwardEisenhart.com
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Trying to new up an instance causes a compiler error
+            //MySingleton compilerError = new MySingleton();
+
+
+            MySingleton s = MySingleton.Instance; // retrieve the only instance using static property (global scope)
+            MySingleton s2 = MySingleton.Instance; // This will grab the exact same instance as 's'
+            s.ExampleMethod(); // Now we can use the instance
+        }
+    }
+}
